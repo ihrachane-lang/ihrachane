2e654494d243dae11dc95491188d344f289e369d
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-09-13 20:13:13 +0600

    ad

--- src/components/shared/NavbarClient.jsx
+++ src/components/shared/NavbarClient.jsx
@@ -100,7 +100,7 @@ export default function NavbarClient({ menus }) {
 
             {/* Mobile menu button */}
             <div className="md:hidden flex items-center">
-              <button className="bg-black px-3 py-1 rounded-xl text-white">Get Offer</button>
+              <button onClick={goToContact} className="bg-black px-3 py-1 rounded-xl text-white">Get Offer</button>
               <button
                 onClick={toggleMenu}
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-orange-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500 transition-colors duration-200"
