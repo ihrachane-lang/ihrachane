ccee0a27f2fd38bd65866ff835b5924aec8872dc
Mohammad Atikuzzaman <akash203037@gmail.com>
2026-08-07 02:01:03 +0600

    connection time modified

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -1,7 +1,6 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
   auth: {
     user: process.env.SMTP_GMAIL_USER,
     pass: process.env.SMTP_GMAIL_PASS,
